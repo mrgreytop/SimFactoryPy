e1700bf13663ac884a041cbb7653e6542e8a0ad8
mrgreytop <36601066+mrgreytop@users.noreply.github.com>
2021-03-02 23:54:58 +0000

    add object extra kw to logger

--- SimFactoryPy/simulation/Loggers.py
+++ SimFactoryPy/simulation/Loggers.py
@@ -33,18 +33,22 @@ class SimLoggerAdapter(logging.LoggerAdapter):
 
     def __init__(self, logger, extra):
         try:
-            env = extra["env"]
+            self.env = extra["env"]
         except KeyError:
             raise ValueError("Must supply a simpy.Environment to extra args")
 
-        if not isinstance(env, simpy.Environment):
+        if not isinstance(self.env, simpy.Environment):
             raise TypeError("env in extra must be a simpy.Environment")
 
+        self.object = extra.get("object")
         super().__init__(logger, extra)
 
-    def process(self, msg, kwargs):
-        env:simpy.Environment = self.extra["env"]
-        return f"{env.now:.3f}: {msg}", kwargs
+    def process(self, msg, kwargs)->tuple:
+        if self.object == None:
+            return f"{self.env.now:.3f}: {msg}", kwargs
+        else:
+            return f"{self.env.now:.3f}: {self.object}: {msg}", kwargs
+            
 
 def parse_env_time(message):
     return float(message.split(":")[0])
